99aad0766be921cceeea0b6fd326c3f6be7d21cd
Annas926 <najibpetikpll@gmail.com>
2024-11-04 09:26:41 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="col-sm-12 col-md-6 offset-md-6 px-md-0">
+              <p class="tm-copyright-text mb-0 tm-text-primary">
+                Copyright &copy; 2019 Company Name 
+                
+                - Design: <a rel="nofollow" href="https://www.facebook.com/templatemo">TemplateMo</a>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- /.columns-bg -->
+  </body>
+</html>
